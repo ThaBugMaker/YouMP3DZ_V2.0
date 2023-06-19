0da137838d88d5c6419b80a57f209dc66b47cf2b
Khalil <118787817+ThaBugMaker@users.noreply.github.com>
2023-06-19 06:48:10 +0100

    Update rateLimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 // Create the rate limiter middleware
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 16, // limit each IP to 15 requests per windowMs,
+  max: 50, // limit each IP to 15 requests per windowMs,
   message: "Rate limit exceeded. Please try again later.",
   // headers: true,
 });
