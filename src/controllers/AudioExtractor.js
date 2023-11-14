b7fa88c6aa75d5b37d6716613c717820193f5646
Khalil <118787817+ThaBugMaker@users.noreply.github.com>
2023-11-14 07:40:55 +0100

    Update AudioExtractor.js
    refactor: Improve YouTube audio extraction URL handling
    
    - Updated the `extractAudio` function to enhance URL handling for improved compatibility.
    - Removed unnecessary console log statement for "Extracting audio started..." to reduce noise.
    - Refactored the URL processing logic to handle cases where 'v=' is present (e.g., in a watch URL) or absent (e.g., in a non-watch URL).
    - Improved code readability and maintained the use of the highest audio quality option in the `ytdl` call.
    
    These changes provide a more robust and streamlined approach to extracting audio from YouTube URLs.

--- src/controllers/AudioExtractor.js
+++ src/controllers/AudioExtractor.js
@@ -1,7 +1,18 @@
 const ytdl = require("ytdl-core");
 
 async function extractAudio(url) {
-  console.log("Extracting audio started...");
+  // console.log("Extracting audio started...");
+  let urlParts;
+  // Updating the url...
+  if (url.includes("v=")) {
+    // Case where 'v=' is present, like in a watch URL
+    urlParts = url.split("?v=");
+    url = `${urlParts[0]}?v=${urlParts[1].split("&si=")[0]}`;
+  } else {
+    // Case where 'v=' is not present, like in a non-watch URL
+    urlParts = url.split("?");
+    url = urlParts[0];
+  }
   return (audioStream = ytdl(url, { quality: "highestaudio" }));
 }
 
