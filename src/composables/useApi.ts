8315631851da2cb97a456486fde35a0b9bbb34e4
ThaBugMaker <KhalilDev@duck.com>
2025-01-07 17:23:45 +0100

    Fixed issues, Removed Hard Coded Api Key (i changed it wont work), implemented Remaining Downloads update.

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,16 +1,35 @@
 // useApi.ts (composable)
+interface Res extends Response {
+  message: string;
+  remainingDownloads: number;
+  mp3_file: string;
+}
+export interface apiRes {
+  url: string;
+  error?: any;
+}
+export async function useApi(url: string): Promise<apiRes | undefined> {
+  let mp3_file: string;
+  let remainingDownloads: number;
+  try {
+    const response = await fetch(`/api/download`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "API-KEY": `${import.meta.env.VITE_API_KEY}`,
+        "APP-NAME": `${import.meta.env.VITE_APP_NAME}`,
+      },
+      body: JSON.stringify({ url }),
+    });
 
-export async function useApi(url: string) {
-  const response = await fetch("http://127.0.0.1:5000/download", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      message: "hi",
-      "API-KEY": "ecb6103eef1a6a368c97cd62ee9f9127",
-    },
-    body: JSON.stringify({ url }),
-  });
-
-  const data = await response.json();
-  console.log(data);
+    const data = (await response.json()) as Res;
+    mp3_file = data.mp3_file;
+    remainingDownloads = data.remainingDownloads;
+    localStorage.setItem("storedRemaining", remainingDownloads.toString());
+    console.info(data);
+    return { url: mp3_file };
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 }
