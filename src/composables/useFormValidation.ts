667b31490aeba1c0d06eddea3cbd694556c5e5f9
ThaBugMaker <KhalilDev@duck.com>
2025-01-07 17:24:01 +0100

    Proper Validations

--- src/composables/useFormValidation.ts
+++ src/composables/useFormValidation.ts
@@ -1,11 +1,18 @@
 import { extractYouTubeVideoID } from "@/utils/utils";
 
-export const validateUrl = (url: string) => {
+const validateUrl = (url: string) => {
   const videoInfo = extractYouTubeVideoID(url);
-  if (!videoInfo) {
-    console.log("Invalid YouTube URL");
+  if (videoInfo.includes("Invalid YouTube URL")) {
     return false;
   } else {
-    return true;
+    return videoInfo;
   }
+};
+
+// Validate if the YouTube video ID is valid (length of 11)
+export const validateID = (url: string) => {
+  const videoID = validateUrl(url); // Get the video ID from the URL
+  if (!videoID) return false; // Return false if URL is invalid
+  if (videoID.length !== 11) return false; // Return false if video ID length is not 11
+  return videoID; // Return the valid video ID
 };
